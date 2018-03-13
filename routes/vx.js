9aca333503a0a77b30effe83af07e1d058add281
Yu Yong <jason.yu4@honeywell.com>
2018-03-13 17:55:26 +0800

    t

--- routes/vx.js
+++ routes/vx.js
@@ -4,7 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.send("respond with a resource11111111");
+	var signature=req.query['signature'];
+	var timestamp=req.query['timestamp'];	
+	var nonce=req.query['nonce'];	
+	var echostr=req.query['echostr'];	
+
+	
+  res.send(echostr);
 };
 
 exports.msg = function(req, res){
